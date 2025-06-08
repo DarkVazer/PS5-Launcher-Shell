a0bee22943f4a3e3a188a2be4ce2738cb9896eef
DarkVazer <110217958+DarkVazer@users.noreply.github.com>
2025-06-08 10:44:31 +0300

    Update LauncherApp.jsx

--- src/LauncherApp.jsx
+++ src/LauncherApp.jsx
@@ -1,4 +1,6 @@
+// LauncherApp.jsx
 import { createRoot } from 'react-dom/client';
 import Launcher from './Launcher.jsx';
+import './App.css'; // Подключаем Tailwind и кастомные стили
 
 createRoot(document.getElementById('launcher-root')).render(<Launcher />);
