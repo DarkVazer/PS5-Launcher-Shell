a6d1759e00ca724853545de4b698412fafe1f889
DarkVazer <110217958+DarkVazer@users.noreply.github.com>
2025-06-08 18:51:44 +0300

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -3,6 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   getGames: () => ipcRenderer.invoke('get-games'),
   addGame: (game) => ipcRenderer.invoke('add-game', game),
-  launchShell: () => ipcRenderer.invoke('launch-shell'),
-  launchGame: (executablePath) => ipcRenderer.invoke('launch-game', executablePath),
+  launchShell: () => ipcRenderer.send('launch-shell'), // Изменено на send (синхронный)
+  launchGame: (executablePath) => ipcRenderer.send('launch-game', executablePath), // Изменено на send
 });
